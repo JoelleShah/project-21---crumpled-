74a8052f9794310232a141681c0b872686e2586a
JoelleShah <121088109+JoelleShah@users.noreply.github.com>
2023-04-11 16:54:28 +1000

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,22 +1,14 @@
-class Ground {
-  constructor(x, y, w, h) {
-    let options = {
-      isStatic:true
-    };
-    
-    this.body = Bodies.rectangle(x, y, w, h, options);
-    this.w = w;
-    this.h = h;
-    World.add(world, this.body);
-  }
-
-  show() {
-    var pos = this.body.position;
-    push();
-    rectMode(CENTER);
-    stroke(255);
-    rect(pos.x, pos.y, this.w, this.h);
-    pop();
-  }
-  
+class Ground{
+    constructor(x,y,w,h){
+        var options={isStatic:true};
+        this.w=w;
+        this.h=h;
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        fill("yellow")
+        rect(pos.x,pos.y,this.w,this.h);
+    }
 }
